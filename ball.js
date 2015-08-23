441c281b03dd7a176f95c7433749c2fa816e1c02
Teemu Sarapisto <tsarpf@gmail.com>
2015-08-23 22:30:51 +0300

    Stop ball to slot

--- ball.js
+++ ball.js
@@ -1,1 +1,23 @@
-this.entity.rigidbody.applyForce(0, 20, 0);
+pc.script.create("balls", function (app) {
+    var ScriptObject = function (entity) {
+        this.entity = entity;
+    };
+
+    var first = true;
+    ScriptObject.prototype = {
+        initialize: function () {
+        },
+
+        update: function (dt) {
+            if (this.entity.getPosition().z < 2.0) {
+                this.entity.rigidbody.applyForce(0, 0, dt * 400);
+            } else if (first){
+                console.log('applied');
+                this.entity.rigidbody.applyImpulse(0, 0, -10);
+                first = false;
+            }
+        }
+    };
+
+    return ScriptObject;
+});
